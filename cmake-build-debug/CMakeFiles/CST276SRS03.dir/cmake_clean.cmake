4ae993881eba7329f7859702ce0712cd8ee6ef71
wolf <johnkaye91@gmail.com>
2018-07-30 15:08:34 -0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/CST276SRS03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CST276SRS03.dir/main.cpp.o"
+  "CST276SRS03.pdb"
+  "CST276SRS03"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CST276SRS03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
